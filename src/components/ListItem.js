f23fe9dab38f51c813d738a36595d47b1f890fb3
MohandZenati <81512623+MohandZenati@users.noreply.github.com>
2023-03-30 18:34:07 +0200

    Update ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,51 +1,29 @@
-import React, {useEffect, useReducer, useState} from 'react';
-
+import React from 'react';
 import { FaTrash } from "react-icons/fa";
 
+function ListItem({ id, nom, prenom, datee, horaire, place, prix, onDelete }) {
 
-
-function ListItem({id, nom, prenom, datee, horaire,  place, prix, handleDelete}) {
-  let [tickets, setTickets] = useState(null)
-
-
-  useEffect(() => {
-    fetch("http://localhost/billeteriee/api/lire.php")
-      .then(response => response.json())
-      .then(data => setTickets(data.billets))
-  }, []) // ajout de "refresh" ici
-  
-
-
- 
-
-function handleClick() {
-  fetch(`http://localhost/billeteriee/api/supprimer.php?id=${id}`, { method: 'DELETE' })
-    .then(() => {
-    
-    
-    })
-    .catch((error) => {
-      console.error('Failed to delete element:', error);
-    });
-}
-
+  function handleClick() {
+    fetch(`https://bubble9.zenati.butmmi.o2switch.site/api/supprimer.php?id=${id}`, { method: 'DELETE' })
+      .then(() => {
+        onDelete();
+      })
+      .catch((error) => {
+        console.error('Failed to delete element:', error);
+      });
+  }
 
   return (
-
-    
-        <tr>
-        <td>Ticket n°{id}</td>
-          <td>{nom} {prenom}</td>
-          <td>{datee}</td>
-          <td>{horaire}</td>
-          <td>{place}</td>
-          <td>{prix}€</td>
-          
-          <td onClick={() => { handleClick(); window.location.reload(true); }}><FaTrash /></td>
-
-
-        </tr>
-  )
+    <tr>
+      <td>Ticket n°{id}</td>
+      <td>{nom} {prenom}</td>
+      <td>{datee}</td>
+      <td>{horaire}</td>
+      <td>{place}</td>
+      <td>{prix}€</td>
+      <td onClick={handleClick}><FaTrash /></td>
+    </tr>
+  );
 }
 
-export default ListItem
+export default ListItem;
