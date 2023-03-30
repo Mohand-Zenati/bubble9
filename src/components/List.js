bbfae8dcd97b88ae5174c1289d3b64d5de11f10b
MohandZenati <81512623+MohandZenati@users.noreply.github.com>
2023-03-30 18:37:58 +0200

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -5,45 +5,63 @@ import ListItem from './ListItem';
 function List() {
   let [tickets, setTickets] = useState(null)
 
+  useEffect(() => {
+    fetch("https://bubble9.zenati.butmmi.o2switch.site/api/lire.php")
+      .then(response => response.json())
+      .then(data => setTickets(data.billets))
+  }, [])
 
-  
-  function handleDelete() {
-
+  function refreshTickets() {
+    fetch("https://bubble9.zenati.butmmi.o2switch.site/api/lire.php")
+      .then(response => response.json())
+      .then(data => setTickets(data.billets))
   }
 
-  useEffect(() => {
-    fetch("http://localhost/billeteriee/api/lire.php")
-    .then(response => response.json())
-    .then(data => setTickets(data.billets))
-
-  
-  },[])
+  function handleDelete(id) {
+    fetch(`https://bubble9.zenati.butmmi.o2switch.site/api/supprimer.php?id=${id}`, { method: 'DELETE' })
+      .then(() => {
+        window.location.reload()
+      })
+      .catch((error) => {
+        console.error('Failed to delete element:', error);
+      });
+  }
 
   return (
   <div> 
-    <div class='table-card'>
-    <h2>Information</h2>
-    <table class='table'>
-      <thead>
-        <tr>
-          <td id='tour-table'>Ticket</td>
-          <td>nom</td>
-          <td>Date de réservation</td>
-          <td>Horaire</td>
-          <td>Nombre de place</td>
-          <td>Prix total</td>
-        </tr>
-      </thead>
-      <tbody>
-        {
-        
-            tickets && tickets.map((item, index) => ( <ListItem key={index} id={item.id} nom={item.nom} prenom={item.prenom} datee={item.datee} horaire={item.horaire}   place={item.place}  prix={item.prix} onDelete={handleDelete} />))
-        }
-         
-      </tbody>
-    </table>
+      <div class='table-card'>
+        <h2>Information</h2>
+        <table class='table'>
+          <thead>
+            <tr>
+              <td id='tour-table'>Ticket</td>
+              <td>nom</td>
+              <td>Date de réservation</td>
+              <td>Horaire</td>
+              <td>Nombre de place</td>
+              <td>Prix total</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              tickets && tickets.map((item, index) => (
+                <ListItem
+                  key={index}
+                  id={item.id}
+                  nom={item.nom}
+                  prenom={item.prenom}
+                  datee={item.datee}
+                  horaire={item.horaire}
+                  place={item.place}
+                  prix={item.prix}
+                  handleDelete={() => handleDelete(item.id)}
+                />
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
   </div>
-</div>
   )
 }
 
